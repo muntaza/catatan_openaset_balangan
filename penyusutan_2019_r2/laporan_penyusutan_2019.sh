ca4d56cd3de4be1e93d623e52e551dc199705bfa
Muhammad Muntaza <muhammad@muntaza.id>
2020-01-12 20:39:02 +0800

    OK

--- penyusutan_2019_r2/laporan_penyusutan_2019.sh
+++ penyusutan_2019_r2/laporan_penyusutan_2019.sh
@@ -6,20 +6,20 @@ cat beban_penyusutan_gb_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g 
 cat beban_penyusutan_jij_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > beban_penyusutan_jij_2019.xml
 cat beban_penyusutan_pm_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > beban_penyusutan_pm_2019.xml
 
-cat penyusutan_gb_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_gb_2018.xml
 cat penyusutan_gb_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_gb_2019.xml
+cat penyusutan_gb_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_gb_2018.xml
 
-cat penyusutan_gb_rinci_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_gb_rinci_2018.xml
 cat penyusutan_gb_rinci_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_gb_rinci_2019.xml
+cat penyusutan_gb_rinci_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_gb_rinci_2018.xml
 
-cat penyusutan_jij_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_jij_2018.xml
 cat penyusutan_jij_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_jij_2019.xml
+cat penyusutan_jij_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_jij_2018.xml
 
-cat penyusutan_jij_rinci_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_jij_rinci_2018.xml
 cat penyusutan_jij_rinci_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_jij_rinci_2019.xml
+cat penyusutan_jij_rinci_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_jij_rinci_2018.xml
 
-cat penyusutan_pm_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_pm_2018.xml
 cat penyusutan_pm_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_pm_2019.xml
+cat penyusutan_pm_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_pm_2018.xml
 
-cat penyusutan_pm_rinci_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_pm_rinci_2018.xml
 cat penyusutan_pm_rinci_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_pm_rinci_2019.xml
+cat penyusutan_pm_rinci_2017.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_pm_rinci_2018.xml

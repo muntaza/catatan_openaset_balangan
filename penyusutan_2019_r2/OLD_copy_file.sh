827803664131f350ca5be8c6164078a7b8ee0bf4
Muhammad Muntaza <muhammad@muntaza.id>
2020-01-12 14:27:41 +0800

    modified:   OLD_copy_file.sh edit tahun
    	new file:   gabung_penyusutan_2017.sh
    add
    
    	new file:   gabung_penyusutan_2018.sh
    add
    
    	modified:   readme
    edit

--- penyusutan_2019_r2/OLD_copy_file.sh
+++ penyusutan_2019_r2/OLD_copy_file.sh
@@ -1,15 +1,21 @@
-cat beban_gb_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > beban_gb_2018_r2.sql
-cat beban_jij_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > beban_jij_2018_r2.sql
-cat beban_pm_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > beban_pm_2018_r2.sql
-cat penyusutan_gb_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_gb_2018_r2.sql
-cat penyusutan_jij_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_jij_2018_r2.sql
-cat penyusutan_pm_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_pm_2018_r2.sql
-cat penyusutan_modul2_gb_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul2_gb_2018_r2.sql
-cat penyusutan_modul2_jij_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul2_jij_2018_r2.sql
-cat penyusutan_modul2_pm_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul2_pm_2018_r2.sql
-cat penyusutan_modul3_gb_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul3_gb_2018_r2.sql
-cat penyusutan_modul3_jij_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul3_jij_2018_r2.sql
-cat penyusutan_modul3_pm_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul3_pm_2018_r2.sql
-cat penyusutan_modul4_gb_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul4_gb_2018_r2.sql
-cat penyusutan_modul4_jij_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul4_jij_2018_r2.sql
-cat penyusutan_modul4_pm_2017_r2.sql | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  | sed -e s/2016/2017/g  > penyusutan_modul4_pm_2018_r2.sql
+#!/bin/bash
+#edit file penyusutan ke tahun 2019
+
+
+
+cat beban_gb_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > beban_gb_2019_r2.sql
+cat beban_jij_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > beban_jij_2019_r2.sql
+cat beban_pm_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > beban_pm_2019_r2.sql
+cat penyusutan_gb_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_gb_2019_r2.sql
+cat penyusutan_jij_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_jij_2019_r2.sql
+cat penyusutan_pm_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_pm_2019_r2.sql
+cat penyusutan_modul2_gb_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul2_gb_2019_r2.sql
+cat penyusutan_modul2_jij_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul2_jij_2019_r2.sql
+cat penyusutan_modul2_pm_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul2_pm_2019_r2.sql
+cat penyusutan_modul3_gb_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul3_gb_2019_r2.sql
+cat penyusutan_modul3_jij_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul3_jij_2019_r2.sql
+cat penyusutan_modul3_pm_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul3_pm_2019_r2.sql
+cat penyusutan_modul4_gb_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul4_gb_2019_r2.sql
+cat penyusutan_modul4_jij_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul4_jij_2019_r2.sql
+cat penyusutan_modul4_pm_2018_r2.sql | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > penyusutan_modul4_pm_2019_r2.sql
+cat gabung_penyusutan_2018.sh | sed -e s/2018/2019/g > gabung_penyusutan_2019.sh
---
+++ penyusutan_2019_r2/gabung_penyusutan_2017.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#Gabung semua file penyusutan 2017
+
+cat penyusutan_gb_2017_r2.sql > penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_jij_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_pm_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul2_gb_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul2_jij_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul2_pm_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul3_gb_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul3_jij_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul3_pm_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul4_gb_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul4_jij_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat penyusutan_modul4_pm_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat beban_gb_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat beban_jij_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql
+cat beban_pm_2017_r2.sql >> penyusutan_gabungan_2017_r2_OK.sql

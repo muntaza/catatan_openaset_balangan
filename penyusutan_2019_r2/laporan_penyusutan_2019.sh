59332a59f29d0b2c099a80a3bcf40dfdad6af53f
Muhammad Muntaza <muhammad@muntaza.id>
2020-01-12 14:38:28 +0800

    edit

---
+++ penyusutan_2019_r2/laporan_penyusutan_2019.sh
@@ -0,0 +1,3 @@
+cat beban_penyusutan_gb_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > beban_penyusutan_gb_2019.xml
+cat beban_penyusutan_jij_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > beban_penyusutan_jij_2019.xml
+cat beban_penyusutan_pm_2018.xml | sed -e s/2019/2020/g  | sed -e s/2018/2019/g  | sed -e s/2017/2018/g  > beban_penyusutan_pm_2019.xml

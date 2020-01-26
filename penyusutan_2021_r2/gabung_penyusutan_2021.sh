b5a3715308d222dff3a185ae7296343850fadd33
Muhammad Muntaza <muhammad@muntaza.id>
2020-01-26 21:32:42 +0800

    penyusutan 2021

---
+++ penyusutan_2021_r2/gabung_penyusutan_2021.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#Gabung semua file penyusutan 2021
+
+cat penyusutan_gb_2021_r2.sql > penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_jij_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_pm_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul2_gb_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul2_jij_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul2_pm_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul3_gb_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul3_jij_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul3_pm_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul4_gb_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul4_jij_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat penyusutan_modul4_pm_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat beban_gb_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat beban_jij_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql
+cat beban_pm_2021_r2.sql >> penyusutan_gabungan_2021_r2_OK.sql

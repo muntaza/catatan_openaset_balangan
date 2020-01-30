dca295fc4a85bd27b994261a6dc4a5aa2cceee54
Muhammad Muntaza <muhammad@muntaza.id>
2020-01-30 21:14:41 +0800

    atl OK

---
+++ refactoring_2020/laporan_kabupaten/atl.sql
@@ -0,0 +1,112 @@
+CREATE OR REPLACE VIEW view_atl_kabupaten AS
+
+SELECT
+atl.id_sub_skpd,
+
+sub_skpd.nama_sub_skpd,
+sub_skpd.id_skpd,
+
+skpd.nama_skpd,
+skpd.id_lokasi_bidang,
+
+lokasi_bidang.nama_lokasi_bidang,
+lokasi_bidang.id_kabupaten,
+
+kabupaten.nama_kabupaten,
+kabupaten.id_provinsi,
+
+provinsi.nama_provinsi,
+
+atl.id_mutasi_berkurang,
+mutasi_berkurang.mutasi_berkurang,
+
+keadaan_barang.keadaan_barang,
+satuan_barang.satuan_barang,
+golongan_barang.golongan_barang,
+
+atl.nama_barang,
+LEFT(kode_barang.kode_barang, 14) kode_barang,
+atl.id register,
+
+atl.judul_pencipta_buku,
+atl.spesifikasi_buku,
+
+atl.asal_daerah_barang_seni,
+atl.pencipta_barang_seni,
+atl.bahan_barang_seni,
+
+atl.jenis_hewan_tumbuhan,
+atl.ukuran_hewan_tumbuhan,
+
+atl.tahun,
+
+array_to_string(array_unique(array_agg(asal_usul.asal_usul)), ', ') asal_usul,
+SUM(harga_atl.harga_bertambah) - SUM(harga_atl.harga_berkurang) harga,
+atl.keterangan
+
+
+FROM
+atl as atl, harga_atl as harga_atl, kode_barang,
+mutasi_berkurang, asal_usul, keadaan_barang, satuan_barang, golongan_barang,
+sub_skpd, skpd, lokasi_bidang, kabupaten, provinsi
+
+
+WHERE
+1 = 1  AND
+harga_atl.id_atl = atl.id AND
+harga_atl.id_asal_usul = asal_usul.id AND
+
+atl.id_kode_barang = kode_barang.id AND
+atl.id_mutasi_berkurang = mutasi_berkurang.id AND
+
+atl.id_keadaan_barang = keadaan_barang.id AND
+atl.id_satuan_barang = satuan_barang.id AND
+atl.id_golongan_barang = golongan_barang.id AND
+
+atl.id_sub_skpd = sub_skpd.id AND
+sub_skpd.id_skpd = skpd.id AND
+skpd.id_lokasi_bidang = lokasi_bidang.id AND
+lokasi_bidang.id_kabupaten = kabupaten.id AND
+kabupaten.id_provinsi = provinsi.id
+
+
+GROUP BY
+atl.id_sub_skpd,
+sub_skpd.nama_sub_skpd,
+sub_skpd.id_skpd,
+skpd.nama_skpd,
+skpd.id_lokasi_bidang,
+lokasi_bidang.nama_lokasi_bidang,
+lokasi_bidang.id_kabupaten,
+kabupaten.nama_kabupaten,
+kabupaten.id_provinsi,
+provinsi.nama_provinsi,
+atl.id_mutasi_berkurang,
+mutasi_berkurang.mutasi_berkurang,
+keadaan_barang.keadaan_barang,
+satuan_barang.satuan_barang,
+golongan_barang.golongan_barang,
+atl.nama_barang,
+kode_barang,
+register,
+
+atl.judul_pencipta_buku,
+atl.spesifikasi_buku,
+
+atl.asal_daerah_barang_seni,
+atl.pencipta_barang_seni,
+atl.bahan_barang_seni,
+
+atl.jenis_hewan_tumbuhan,
+atl.ukuran_hewan_tumbuhan,
+
+atl.tahun,
+atl.keterangan
+
+ORDER BY id_skpd, kode_barang;
+
+
+
+
+GRANT ALL PRIVILEGES ON view_atl_kabupaten TO lap_kabupaten;
+REVOKE INSERT, UPDATE, DELETE ON view_atl_kabupaten FROM lap_kabupaten;
--- refactoring_2020/laporan_kabupaten/script.sh
+++ refactoring_2020/laporan_kabupaten/script.sh
@@ -18,6 +18,10 @@ cat jalan_irigasi_jaringan.sql | sed -e s/kode_barang/kode_barang_108/g | \
 	sed -e s/view_jalan_irigasi_jaringan_kabupaten/view_jalan_irigasi_jaringan_108_kabupaten/g | \
         sed -e s/14/18/g   > jalan_irigasi_jaringan_108.sql
 
+cat atl.sql | sed -e s/kode_barang/kode_barang_108/g | \
+	sed -e s/view_atl_kabupaten/view_atl_108_kabupaten/g | \
+        sed -e s/14/18/g   > atl_108.sql
+
 
 
 

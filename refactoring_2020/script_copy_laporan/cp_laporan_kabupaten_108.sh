539427471fd611800952764c3c0524bec730a4b6
Muhammad Muntaza <muhammad@muntaza.id>
2020-02-15 11:45:13 +0800

    UPDATE_laporan_108

--- refactoring_2020/script_copy_laporan/cp_laporan_kabupaten_108.sh
+++ refactoring_2020/script_copy_laporan/cp_laporan_kabupaten_108.sh
@@ -40,3 +40,8 @@ cat /var/www/html/php5_laporan/php5_projects/kabupaten/kdp_jij_skpd.xml  | \
 	sed -e s/view_jalan_irigasi_jaringan_kabupaten/view_jalan_irigasi_jaringan_108_kabupaten/g  \
         > kdp_jalan_irigasi_jaringan_skpd_108.xml
 
+cat /var/www/html/php5_laporan/php5_projects/kabupaten/buku_inventaris_skpd.xml  | \
+	sed -e s/kode_barang/kode_barang_108/g | \
+	sed -e s/view_buku_inventaris_aset_tetap_l2/view_buku_inventaris_108_kabupaten/g  \
+        > buku_inventaris_skpd_108.xml
+

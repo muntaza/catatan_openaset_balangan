a71294958738c114947f30a4dc8f7f0fffbfda05
Muhammad Muntaza <muhammad@muntaza.id>
2020-02-16 10:52:29 +0800

    UPDATE RELEASE

--- refactoring_2020/script_copy_laporan/cp_laporan_setwan_108.sh
+++ refactoring_2020/script_copy_laporan/cp_laporan_setwan_108.sh
@@ -7,42 +7,42 @@
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/tanahskpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_tanah_kabupaten/view_tanah_108_kabupaten/g  \
+	sed -e s/view_tanah_setwan/view_tanah_108_setwan/g  \
         > tanah_skpd_108.xml
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/peralatanmesinskpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_peralatan_mesin_kabupaten/view_peralatan_mesin_108_kabupaten/g  \
+	sed -e s/view_peralatan_mesin_setwan/view_peralatan_mesin_108_setwan/g  \
         > peralatan_mesin_skpd_108.xml
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/gedungskpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_gedung_bangunan_kabupaten/view_gedung_bangunan_108_kabupaten/g  \
+	sed -e s/view_gedung_bangunan_setwan/view_gedung_bangunan_108_setwan/g  \
         > gedung_bangunan_skpd_108.xml
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/jij_skpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_jalan_irigasi_jaringan_kabupaten/view_jalan_irigasi_jaringan_108_kabupaten/g  \
+	sed -e s/view_jalan_irigasi_jaringan_setwan/view_jalan_irigasi_jaringan_108_setwan/g  \
         > jalan_irigasi_jaringan_skpd_108.xml
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/atl_skpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_atl_kabupaten/view_atl_108_kabupaten/g  \
+	sed -e s/view_atl_setwan/view_atl_108_setwan/g  \
         > atl_skpd_108.xml
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/kdp_gedung_skpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_gedung_bangunan_kabupaten/view_gedung_bangunan_108_kabupaten/g  \
+	sed -e s/view_gedung_bangunan_setwan/view_gedung_bangunan_108_setwan/g  \
         > kdp_gedung_bangunan_skpd_108.xml
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/kdp_jij_skpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_jalan_irigasi_jaringan_kabupaten/view_jalan_irigasi_jaringan_108_kabupaten/g  \
+	sed -e s/view_jalan_irigasi_jaringan_setwan/view_jalan_irigasi_jaringan_108_setwan/g  \
         > kdp_jalan_irigasi_jaringan_skpd_108.xml
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/buku_inventaris_skpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_buku_inventaris_aset_tetap_l2/view_buku_inventaris_108_kabupaten/g | \
+	sed -e s/view_buku_inventaris_aset_tetap_l2/view_buku_inventaris_108/g | \
 	sed -e s/kode_level_2/kode_level_2_108/g  \
         > buku_inventaris_skpd_108.xml
 

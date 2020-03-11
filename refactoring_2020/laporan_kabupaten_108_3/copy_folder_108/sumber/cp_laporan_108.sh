e7b0d006d8d1d38f0120b0c7c2321f6bc3547d0e
Muhammad Muntaza <muhammad@muntaza.id>
2020-03-11 11:35:49 +0800

    OK

--- refactoring_2020/laporan_kabupaten_108_3/copy_folder_108/sumber/cp_laporan_108.sh
+++ refactoring_2020/laporan_kabupaten_108_3/copy_folder_108/sumber/cp_laporan_108.sh
@@ -8,13 +8,13 @@
 
 cat /var/www/html/php5_laporan/php5_projects/kabupaten/extracomptable_skpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_extracomptable_l2/view_extracomptable_108_kabupaten/g | \
+	sed -e s/view_extracomptable/view_extracomptable_108_kabupaten/g | \
 	sed -e s/kode_level_2/kode_level_2_108/g  \
         > /var/www/html/php5_laporan_108/php5_projects_108/kabupaten/extracomptable_skpd_108.xml
 
 
 cat /var/www/html/php5_laporan/php5_projects/01_setwan/extracomptable_skpd.xml  | \
 	sed -e s/kode_barang/kode_barang_108/g | \
-	sed -e s/view_extracomptable_l2_setwan/view_extracomptable_108_setwan/g | \
+	sed -e s/view_extracomptable_setwan/view_extracomptable_108_setwan/g | \
 	sed -e s/kode_level_2/kode_level_2_108/g  \
         > /var/www/html/php5_laporan_108/php5_projects_108/01_setwan/extracomptable_skpd_108.xml

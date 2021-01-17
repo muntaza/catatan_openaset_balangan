19049604eadd2dd087a92d3bac8532a364a7fddf
Muhammad Muntaza <muhammad@muntaza.id>
2021-01-17 23:54:41 +0800

    update

--- penyusutan_108_2020/kelompok_barang_108/script_rubah.sh
+++ penyusutan_108_2020/kelompok_barang_108/script_rubah.sh
@@ -1,1 +1,2 @@
 cat kelompok_barang_108.sql | sed -e s/^/INSERT\ INTO\ kelompok_barang_108\ \(kode_kelompok_barang\,\ nama_kelompok_barang\)\ VALUES\ \(/g -e s/$/\)\;/g > kelompok_barang_108_insert.sql
+cat penambahan_umur_108.sql | sed -e s/^/INSERT\ INTO\ penambahan_umur_108\ \(id_kelompok_barang_108\,\ persen\,\ umur\)\ VALUES\ \(/g -e s/$/\)\;/g > penambahan_umur_108_insert.sql

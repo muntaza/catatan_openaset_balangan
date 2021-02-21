8883faa2ab887cb73defb527a50c75f605ac4c17
Muhammad Muntaza <muhammad@muntaza.id>
2021-02-21 19:45:28 +0800

    update

---
+++ penyusutan_108_2019/gabung_penyusutan_2019_atl.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Gabung semua file penyusutan 2019
+
+cat penyusutan_atl_2019_r2.sql > penyusutan_atl_gabungan_2019_r2_OK.sql
+cat penyusutan_modul2_atl_2019_r2.sql >> penyusutan_atl_gabungan_2019_r2_OK.sql
+cat penyusutan_modul3_atl_2019_r2.sql >> penyusutan_atl_gabungan_2019_r2_OK.sql
+cat penyusutan_modul4_atl_2019_r2.sql >> penyusutan_atl_gabungan_2019_r2_OK.sql
--- penyusutan_108_2019/penyusutan_atl_gabungan_2019_r2_OK.sql
+++ penyusutan_108_2019/penyusutan_atl_gabungan_2019_r2_OK.sql
@@ -1,3 +1,110 @@
+DROP VIEW IF EXISTS view_penyusutan_108_atl_2019_r2_a1 CASCADE;
+
+
+CREATE VIEW view_penyusutan_108_atl_2019_r2_a1 AS
+
+
+
+SELECT * FROM
+
+(SELECT
+atl.id_sub_skpd,
+
+sub_skpd.nama_sub_skpd,
+sub_skpd.id_skpd,
+
+skpd.nama_skpd,
+skpd.id_lokasi_bidang,
+
+lokasi_bidang.nama_lokasi_bidang,
+lokasi_bidang.id_kabupaten,
+
+kabupaten.nama_kabupaten,
+kabupaten.id_provinsi,
+
+provinsi.nama_provinsi,
+
+atl.id_mutasi_berkurang,
+mutasi_berkurang.mutasi_berkurang,
+
+keadaan_barang.keadaan_barang,
+satuan_barang.satuan_barang,
+golongan_barang.golongan_barang,
+atl.id_golongan_barang,
+
+atl.nama_barang,
+LEFT(kode_barang_108.kode_barang_108, 18) kode_barang_108,
+LEFT(kode_barang_108.kode_barang_108, 8) kode_l2,
+atl.id register,
+
+harga_atl.tahun,
+SUM(harga_atl.harga_bertambah) - SUM(harga_atl.harga_berkurang) harga,
+atl.keterangan
+
+
+
+FROM
+atl as atl, harga_atl as harga_atl, kode_barang_108,
+mutasi_berkurang, asal_usul, keadaan_barang, satuan_barang, golongan_barang,
+sub_skpd, skpd, lokasi_bidang, kabupaten, provinsi
+
+
+WHERE
+1 = 1  AND
+harga_atl.id_atl = atl.id AND
+harga_atl.id_asal_usul = asal_usul.id AND
+harga_atl.tahun <= 2019 AND
+
+atl.id_kode_barang_108 = kode_barang_108.id AND
+atl.id_mutasi_berkurang = mutasi_berkurang.id AND
+
+atl.id_keadaan_barang = keadaan_barang.id AND
+atl.id_satuan_barang = satuan_barang.id AND
+atl.id_golongan_barang = golongan_barang.id AND
+
+golongan_barang.id = 5 AND
+
+atl.id_sub_skpd = sub_skpd.id AND
+sub_skpd.id_skpd = skpd.id AND
+skpd.id_lokasi_bidang = lokasi_bidang.id AND
+lokasi_bidang.id_kabupaten = kabupaten.id AND
+kabupaten.id_provinsi = provinsi.id
+
+
+
+GROUP BY
+atl.id_sub_skpd,
+sub_skpd.nama_sub_skpd,
+sub_skpd.id_skpd,
+skpd.nama_skpd,
+skpd.id_lokasi_bidang,
+lokasi_bidang.nama_lokasi_bidang,
+lokasi_bidang.id_kabupaten,
+kabupaten.nama_kabupaten,
+kabupaten.id_provinsi,
+provinsi.nama_provinsi,
+atl.id_mutasi_berkurang,
+mutasi_berkurang.mutasi_berkurang,
+keadaan_barang.keadaan_barang,
+satuan_barang.satuan_barang,
+golongan_barang.golongan_barang,
+atl.id_golongan_barang,
+atl.nama_barang,
+
+kode_barang_108,
+kode_l2,
+register,
+
+harga_atl.tahun,
+atl.keterangan) AS QUERY_PERALATAN_MESIN
+
+
+
+;
+
+
+GRANT ALL PRIVILEGES ON view_penyusutan_108_atl_2019_r2_a1 TO lap_kabupaten;
+REVOKE INSERT, UPDATE, DELETE ON view_penyusutan_108_atl_2019_r2_a1 FROM lap_kabupaten;
 DROP view if exists view_penyusutan_108_atl_2019_r2_e2 CASCADE;
 
 create view view_penyusutan_108_atl_2019_r2_e2 as select
@@ -2880,3 +2987,102 @@ order by register, rank;
 
 
 -- kunci angka baris ke 10
+DROP view if exists view_penyusutan_108_atl_2019_r2_a3 CASCADE;
+
+create view view_penyusutan_108_atl_2019_r2_a3 as
+
+select register,
+nama_skpd,
+id_skpd,
+nama_lokasi_bidang,
+id_lokasi_bidang,
+nama_kabupaten,
+id_kabupaten,
+nama_provinsi,
+id_provinsi,
+id_mutasi_berkurang,
+mutasi_berkurang,
+keadaan_barang,
+nama_barang,
+
+harga,
+nilai_perolehan,
+sum(harga) over (partition by register) as harga_total,
+tahun,
+kode_barang_108,
+kode_umur,
+rank,
+tahun_akhir - 1 as tahun_akhir,
+persen_awal,
+
+persentasi,
+
+masa_manfaat,
+penambahan_umur,
+
+
+umur_awal,
+sisa_umur,
+nilai_buku_awal,
+penyusutan,
+nilai_buku_akhir
+
+
+ from
+view_penyusutan_108_atl_2019_r2_e29
+
+
+order by register, rank;
+
+GRANT ALL PRIVILEGES ON view_penyusutan_108_atl_2019_r2_a3 TO lap_kabupaten;
+REVOKE INSERT, UPDATE, DELETE ON view_penyusutan_108_atl_2019_r2_a3 FROM lap_kabupaten;
+DROP view if exists view_penyusutan_108_atl_2019_r2_a4 CASCADE;
+
+create view view_penyusutan_108_atl_2019_r2_a4 as
+
+select register,
+nama_skpd,
+id_skpd,
+nama_lokasi_bidang,
+id_lokasi_bidang,
+nama_kabupaten,
+id_kabupaten,
+nama_provinsi,
+id_provinsi,
+id_mutasi_berkurang,
+mutasi_berkurang,
+keadaan_barang,
+nama_barang,
+
+kode_barang_108,
+min(tahun) as tahun_awal,
+
+sum(harga) as nilai_perolehan,
+sum(penyusutan) as penyusutan,
+sum(harga) - sum(penyusutan) as nilai_buku
+
+
+ from
+view_penyusutan_108_atl_2019_r2_a3
+
+GROUP BY
+register,
+nama_skpd,
+id_skpd,
+nama_lokasi_bidang,
+id_lokasi_bidang,
+nama_kabupaten,
+id_kabupaten,
+nama_provinsi,
+id_provinsi,
+id_mutasi_berkurang,
+mutasi_berkurang,
+keadaan_barang,
+nama_barang,
+
+kode_barang_108
+
+order by register;
+
+GRANT ALL PRIVILEGES ON view_penyusutan_108_atl_2019_r2_a4 TO lap_kabupaten;
+REVOKE INSERT, UPDATE, DELETE ON view_penyusutan_108_atl_2019_r2_a4 FROM lap_kabupaten;

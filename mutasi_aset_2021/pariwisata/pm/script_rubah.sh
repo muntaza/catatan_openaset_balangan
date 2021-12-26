57d7a73775a42e05b2fde8dfcb36790cde437fcf
Muhammad Muntaza <muhammad@muntaza.id>
2021-12-26 19:40:19 +0800

    add

--- mutasi_aset_2021/pariwisata/pm/script_rubah.sh
+++ mutasi_aset_2021/pariwisata/pm/script_rubah.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #beri awalan dan akhiran
-cat pm_pariwisata_ke_dinas_pemuda.sql | sed -e s/^/\ \ \ \ clone_object\(PeralatanMesin\.objects\.get\(pk\=/g -e s/$/\)/g > pm_pariwisata_ke_dinas_pemuda_awal.sql
+cat pm_pariwisata_ke_dinas_pemuda.sql | sed -e s/^/\ \ \ \ clone_object\(PeralatanMesin\.objects\.get\(pk\=/g -e s/$/\)\)/g > pm_pariwisata_ke_dinas_pemuda_awal.py
 
 #gabungkan
-cat header_pm.py > pm_pariwisata_ke_dinas_pemuda_insert.sql
-cat ./pm_pariwisata_ke_dinas_pemuda_awal.sql >> pm_pariwisata_ke_dinas_pemuda_insert.sql
+cat header_pm.py > pm_pariwisata_ke_dinas_pemuda_insert.py
+cat ./pm_pariwisata_ke_dinas_pemuda_awal.py >> pm_pariwisata_ke_dinas_pemuda_insert.py

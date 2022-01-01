1cef3f048452c42eeb72b5f0dcbb52b470164f92
Muhammad Muntaza <muhammad@muntaza.id>
2022-01-01 11:50:28 +0800

    ATL Perikanan

--- mutasi_aset_2021/perikanan/atl_perikanan_ke_dinas_ketahanan_pangan/script_rubah.sh
+++ mutasi_aset_2021/perikanan/atl_perikanan_ke_dinas_ketahanan_pangan/script_rubah.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #beri awalan dan akhiran
-cat atl_perikanan_ke_dinas_ketahanan_pangan.sql | sed -e s/^/\ \ \ \ clone_object\(AsetTetapLainnya\.objects\.get\(pk\=/g -e s/$/\)\)/g > atl_perikanan_ke_dinas_ketahanan_pangan_awal.py
+cat atl_perikanan_ke_dinas_ketahanan_pangan.sql | sed -e s/^/\ \ \ \ clone_object\(ATL\.objects\.get\(pk\=/g -e s/$/\)\)/g > atl_perikanan_ke_dinas_ketahanan_pangan_awal.py
 
 #gabungkan
 cat header_atl.py > atl_perikanan_ke_dinas_ketahanan_pangan_insert.py
--- mutasi_aset_2021/perikanan/atl_perikanan_ke_dinas_ketahanan_pangan/sql/reg_baru/script_rubah.sh
+++ mutasi_aset_2021/perikanan/atl_perikanan_ke_dinas_ketahanan_pangan/sql/reg_baru/script_rubah.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 #beri awalan dan akhiran
-cat pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru.sql | sed -e s/^/UPDATE\ peralatan_mesin\ SET\ id_mutasi_berkurang\ \=\ 2\ WHERE\ id\ \=\ /g -e s/$/\;/g > pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_mutasi_berkurang.sql
-cat pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru.sql | sed -e s/^/INSERT\ INTO\ skpd_tujuan_peralatan_mesin\ \(id_peralatan_mesin\,\ id_skpd\)\ VALUES\ \(/g -e s/$/\,\ 13\)\ ON\ CONFLICT\ \(id_peralatan_mesin\)\ DO\ UPDATE\ SET\ id_skpd\ \=\ 13\;/g > pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_skpd_tujuan.sql
-cat pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru.sql | sed -e s/^/INSERT\ INTO\ tahun_berkurang_peralatan_mesin\ \(id_peralatan_mesin\,\ tahun_berkurang\)\ VALUES\ \(/g -e s/$/\,\ 2021\)\ ON\ CONFLICT\ \(id_peralatan_mesin\)\ DO\ UPDATE\ SET\ tahun_berkurang\ \=\ 2021\;/g > pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_tahun_berkurang.sql
+cat atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru.sql | sed -e s/^/UPDATE\ atl\ SET\ id_mutasi_berkurang\ \=\ 2\ WHERE\ id\ \=\ /g -e s/$/\;/g > atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_mutasi_berkurang.sql
+cat atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru.sql | sed -e s/^/INSERT\ INTO\ skpd_tujuan_atl\ \(id_atl\,\ id_skpd\)\ VALUES\ \(/g -e s/$/\,\ 13\)\ ON\ CONFLICT\ \(id_atl\)\ DO\ UPDATE\ SET\ id_skpd\ \=\ 13\;/g > atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_skpd_tujuan.sql
+cat atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru.sql | sed -e s/^/INSERT\ INTO\ tahun_berkurang_atl\ \(id_atl\,\ tahun_berkurang\)\ VALUES\ \(/g -e s/$/\,\ 2021\)\ ON\ CONFLICT\ \(id_atl\)\ DO\ UPDATE\ SET\ tahun_berkurang\ \=\ 2021\;/g > atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_tahun_berkurang.sql
 
 
 #gabungkan
-cat  pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_mutasi_berkurang.sql > pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_insert.sql
-cat  pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_skpd_tujuan.sql >> pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_insert.sql
-cat  pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_tahun_berkurang.sql >> pm_perikanan_ke_dinas_ketahanan_pangan_reg_baru_insert.sql
+cat  atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_mutasi_berkurang.sql > atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_insert.sql
+cat  atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_skpd_tujuan.sql >> atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_insert.sql
+cat  atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_tahun_berkurang.sql >> atl_perikanan_ke_dinas_ketahanan_pangan_reg_baru_insert.sql
